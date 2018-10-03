e372d416907308b653a2ca6b4da3cc3acc634363
Harry.Bartlam <Harry.Bartlam@mubaloo.com>
2018-10-03 10:20:02 +0100

    First commit

---
+++ app/src/main/java/sceneform/offbow/com/sceneform/SceneFoamActivity.kt
@@ -0,0 +1,12 @@
+package sceneform.offbow.com.sceneform
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SceneFoamActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scene_foam)
+    }
+}
